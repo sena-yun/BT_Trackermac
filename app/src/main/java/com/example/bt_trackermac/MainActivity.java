b981221655b3a98d80b7af00b9b6bbb53160f1b1
sena-yun <2020m091@kuas.ac.jp>
2020-05-29 13:57:44 +0900

    styled up button 2,3,4

---
+++ app/src/main/java/com/example/bt_trackermac/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bt_trackermac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToLogActivity(View view) {
+        Intent toLog = new Intent(this,LogActivity.class);
+        startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this,NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this,MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void goToHandleActivity(View view) {
+        Intent toHandle = new Intent(this,HandleActivity.class);
+        startActivity(toHandle);
+    }
+}
