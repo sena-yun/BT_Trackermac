49bda6b9bcf156782b701e808774f31bbdf9be2a
sena-yun <2020m091@kuas.ac.jp>
2020-06-05 14:15:18 +0900

    Set margins for LinearLayout in MainActivity

--- app/src/main/java/com/example/bt_trackermac/MainActivity.java
+++ app/src/main/java/com/example/bt_trackermac/MainActivity.java
@@ -17,22 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void goToLogActivity(View view) {
-        Intent toLog = new Intent(this,LogActivity.class);
+        Intent toLog = new Intent(this, LogActivity.class);
         startActivity(toLog);
     }
 
     public void goToNormalActivity(View view) {
-        Intent toNormal = new Intent(this,NormalActivity.class);
+        Intent toNormal = new Intent(this, NormalActivity.class);
         startActivity(toNormal);
     }
 
     public void goToMechanismActivity(View view) {
-        Intent toMechanism = new Intent(this,MechanismActivity.class);
+        Intent toMechanism = new Intent(this, MechanismActivity.class);
         startActivity(toMechanism);
     }
 
     public void goToHandleActivity(View view) {
-        Intent toHandle = new Intent(this,HandleActivity.class);
+        Intent toHandle = new Intent(this, HandleActivity.class);
         startActivity(toHandle);
     }
 }
