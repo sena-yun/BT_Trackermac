8a58cbf379e53521948a85451983998af9431399
sena-yun <2020m091@kuas.ac.jp>
2020-06-19 12:16:34 +0900

    added log messages in BroadcastReceiver

--- app/src/main/java/com/example/bt_trackermac/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bt_trackermac/ReminderBroadcastReceiver.java
@@ -10,20 +10,28 @@ import android.content.res.Resources;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import android.util.Log;
+
 
 public class ReminderBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        String TAG="onReceive";
+        Intent notificationIntent=new Intent(context, LogActivity.class);
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+        Log.d(TAG, "start building notification");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"BT_Tracker_Channel")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Trakcer")
                 .setContentText("Please log your body temperature now")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Log.d(TAG, "finish building notification");
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        Log.d(TAG, "start firing notification");
         notificationManager.notify(200,builder.build());
+        Log.d(TAG, "notification fired");
     }
 }
